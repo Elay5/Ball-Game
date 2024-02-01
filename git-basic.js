d79c1937bae7e13e1642997cb8bc3f8d4f4998e6
Elay5 <155320852+Elay5@users.noreply.github.com>
2024-02-01 16:21:09 +0200

    Add files via upload

--- git-basic.js
+++ git-basic.js
@@ -2,12 +2,16 @@
 
 var newBallWidth = 150
 var newBallallheight = 150
+var ballSize=100
 function onBallClick() {
     const elBall = document.querySelector(".ball")
+    var ballGrowth =getRandomInt(20,61)
+    var newBallSize=ballSize+ballGrowth
+    elBall.style.backgroundColor= getRandomColor()+''
 
-    if (newBallWidth < 400) {
-        elBall.style.width = newBallWidth + 'px'
-        elBall.style.height = newBallallheight + 'px'
+    if (newBallSize < 400) {
+        elBall.style.width = newBallSize + 'px'
+        elBall.style.height = newBallSize + 'px'
         newBallWidth += 50
         newBallallheight += 50
 
@@ -18,7 +22,29 @@ function onBallClick() {
         newBallWidth = 150
         elBall.style.width = '100px'
         elBall.style.height = '100px'
+        ballSize=100
+        newBallSize=ballSize
     }
+
+    ballSize=newBallSize
+    console.log('size:',ballSize)
+    console.log('new:',newBallSize)
+    console.log('growth:',ballGrowth)
 
 
 }
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
